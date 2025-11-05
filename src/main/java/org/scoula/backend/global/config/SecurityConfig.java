65f2da80efbfcb00dd55c55bd2b4df5f8e6cdafd
juwon1105 <amy1105a@naver.com>
2025-11-05 10:23:32 +0900

    Feat : 주차별 질문 조회 JWT 인증 추가

--- src/main/java/org/scoula/backend/global/config/SecurityConfig.java
+++ src/main/java/org/scoula/backend/global/config/SecurityConfig.java
@@ -1,25 +1,35 @@
 package org.scoula.backend.global.config;
 
+import org.scoula.backend.global.security.JwtAuthenticationFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import lombok.RequiredArgsConstructor;
 
 @Configuration
-@EnableWebSecurity
+@RequiredArgsConstructor
+
 public class SecurityConfig {
+	private final JwtAuthenticationFilter jwtAuthenticationFilter;
+
 
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 		http
 			.csrf(csrf -> csrf.disable()) // Postman 테스트용 CSRF 비활성화
-			.cors(cors -> cors.disable())
 			.authorizeHttpRequests(auth -> auth
-				.requestMatchers("/api/member/register","/api/auth/login","/api/questions/**","/api/video-answers/**" ).permitAll() // ✅ 회원가입은 허용
+				.requestMatchers("/api/member/register", "/api/auth/login").permitAll()
+				// 질문, 영상 답변은 로그인 필요
+				.requestMatchers("/api/questions/**", "/api/video-answers/**").authenticated()
 				.anyRequest().authenticated() // 그 외 요청은 인증 필요
 			)
+			.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
+
 			.httpBasic(httpBasic -> httpBasic.disable()) // REST API는 기본 로그인 비활성화
 			.formLogin(form -> form.disable()); // 폼 로그인 비활성화
 
