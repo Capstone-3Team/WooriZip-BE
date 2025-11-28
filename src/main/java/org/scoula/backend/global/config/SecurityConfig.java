6a18846f30a461248d24fa8cddb1c737f1621f74
juwon1105 <amy1105a@naver.com>
2025-11-28 20:36:59 +0900

    fix : cors 설정 추가

--- src/main/java/org/scoula/backend/global/config/SecurityConfig.java
+++ src/main/java/org/scoula/backend/global/config/SecurityConfig.java
@@ -1,5 +1,7 @@
 package org.scoula.backend.global.config;
 
+import java.util.List;
+
 import org.scoula.backend.global.security.JwtAuthenticationFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -8,6 +10,9 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import lombok.RequiredArgsConstructor;
 
@@ -21,6 +26,7 @@ public class SecurityConfig {
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 		http
+			.cors(cors -> cors.configurationSource(corsConfigurationSource()))   // CORS 활성화
 			.csrf(csrf -> csrf.disable()) // Postman 테스트용 CSRF 비활성화
 			.authorizeHttpRequests(auth -> auth
 				.requestMatchers("/member/register","/home", "/auth/login","/auth/password/**" ,"/v3/api-docs/**",
@@ -37,6 +43,21 @@ public class SecurityConfig {
 
 		return http.build();
 	}
+
+	// CORS 설정
+	@Bean
+	public CorsConfigurationSource corsConfigurationSource() {
+		CorsConfiguration config = new CorsConfiguration();
+		config.setAllowedOrigins(List.of("http://localhost:5173")); // ⭐ 프론트 개발 서버
+		config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+		config.setAllowedHeaders(List.of("*"));
+		config.setAllowCredentials(true);
+
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", config);
+		return source;
+	}
+
 	// ✅ BCryptPasswordEncoder Bean 등록
 	@Bean
 	public BCryptPasswordEncoder passwordEncoder() {
