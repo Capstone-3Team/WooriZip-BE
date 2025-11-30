a7862b6f98e2b34231e105e60d9d7d5da62cae3a
juwon1105 <amy1105a@naver.com>
2025-11-30 14:44:44 +0900

    fix : 비밀번호 변경 스웨거 설정 추가

--- src/main/java/org/scoula/backend/domain/mypage/dto/ChangePasswordRequest.java
+++ src/main/java/org/scoula/backend/domain/mypage/dto/ChangePasswordRequest.java
@@ -1,9 +1,12 @@
 package org.scoula.backend.domain.mypage.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 
 @Getter
 public class ChangePasswordRequest {
+	@Schema(description = "기존 비밀번호", example = "old1234!")
 	private String oldPassword;
+	@Schema(description = "새 비밀번호", example = "new1234!")
 	private String newPassword;
 }
