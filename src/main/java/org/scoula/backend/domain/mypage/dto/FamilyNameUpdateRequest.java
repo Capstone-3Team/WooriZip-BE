dd2d20ae2081014124113e03bd3afd2367dfa2c6
juwon1105 <amy1105a@naver.com>
2025-11-30 11:40:27 +0900

    feat : 가족프로필 추가

---
+++ src/main/java/org/scoula/backend/domain/mypage/dto/FamilyNameUpdateRequest.java
@@ -0,0 +1,8 @@
+package org.scoula.backend.domain.mypage.dto;
+
+import lombok.Getter;
+
+@Getter
+public class FamilyNameUpdateRequest {
+	private String familyName;
+}
---
+++ src/main/java/org/scoula/backend/domain/mypage/dto/FamilyProfileResponse.java
@@ -0,0 +1,24 @@
+package org.scoula.backend.domain.mypage.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.util.List;
+
+@Getter
+@AllArgsConstructor
+public class FamilyProfileResponse {
+
+	private String familyName;
+	private String inviteCode;
+
+	private MemberInfo leader;
+	private List<MemberInfo> members;
+
+	@Getter
+	@AllArgsConstructor
+	public static class MemberInfo {
+		private String nickname;
+		private String profileImage;
+	}
+}
