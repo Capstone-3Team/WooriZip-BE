7ef219c13c0bb7d1644b39b8102b0712f444e3c4
juwon1105 <amy1105a@naver.com>
2025-12-04 20:05:50 +0900

    Fix : 카카오 환경변수 제거

--- src/main/java/org/scoula/backend/domain/kakao/service/KakaoAuthService.java
+++ src/main/java/org/scoula/backend/domain/kakao/service/KakaoAuthService.java
@@ -14,59 +14,59 @@ import java.util.Map;
 @Service
 @RequiredArgsConstructor
 public class KakaoAuthService {
-
-	@Value("${kakao.client-id}")
-	private String clientId;
-
-	@Value("${kakao.redirect-uri}")
-	private String redirectUri;
-
-	@Value("${kakao.token-uri}")
-	private String tokenUri;
-
-	@Value("${kakao.user-info-uri}")
-	private String userInfoUri;
-
-	private final RestTemplate restTemplate = new RestTemplate();
-
-	// 1) Access Token 요청
-	public String requestAccessToken(String code) throws Exception {
-
-		HttpHeaders headers = new HttpHeaders();
-		headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
-
-		MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
-		params.add("grant_type", "authorization_code");
-		params.add("client_id", clientId);
-		params.add("redirect_uri", redirectUri);
-		params.add("code", code);
-
-		HttpEntity<MultiValueMap<String, String>> request =
-			new HttpEntity<>(params, headers);
-
-		ResponseEntity<String> response =
-			restTemplate.postForEntity(tokenUri, request, String.class);
-
-		ObjectMapper mapper = new ObjectMapper();
-		JsonNode node = mapper.readTree(response.getBody());
-
-		return node.get("access_token").asText();
-	}
-
-	// 2) 유저 정보 요청
-	public Map<String, Object> getUserInfo(String accessToken) {
-		HttpHeaders headers = new HttpHeaders();
-		headers.add("Authorization", "Bearer " + accessToken);
-
-		HttpEntity<?> entity = new HttpEntity<>(headers);
-
-		ResponseEntity<Map> response = restTemplate.exchange(
-			userInfoUri,
-			HttpMethod.GET,
-			entity,
-			Map.class
-		);
-
-		return response.getBody();
-	}
+	//
+	// @Value("${kakao.client-id}")
+	// private String clientId;
+	//
+	// @Value("${kakao.redirect-uri}")
+	// private String redirectUri;
+	//
+	// @Value("${kakao.token-uri}")
+	// private String tokenUri;
+	//
+	// @Value("${kakao.user-info-uri}")
+	// private String userInfoUri;
+	//
+	// private final RestTemplate restTemplate = new RestTemplate();
+	//
+	// // 1) Access Token 요청
+	// public String requestAccessToken(String code) throws Exception {
+	//
+	// 	HttpHeaders headers = new HttpHeaders();
+	// 	headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+	//
+	// 	MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
+	// 	params.add("grant_type", "authorization_code");
+	// 	params.add("client_id", clientId);
+	// 	params.add("redirect_uri", redirectUri);
+	// 	params.add("code", code);
+	//
+	// 	HttpEntity<MultiValueMap<String, String>> request =
+	// 		new HttpEntity<>(params, headers);
+	//
+	// 	ResponseEntity<String> response =
+	// 		restTemplate.postForEntity(tokenUri, request, String.class);
+	//
+	// 	ObjectMapper mapper = new ObjectMapper();
+	// 	JsonNode node = mapper.readTree(response.getBody());
+	//
+	// 	return node.get("access_token").asText();
+	// }
+	//
+	// // 2) 유저 정보 요청
+	// public Map<String, Object> getUserInfo(String accessToken) {
+	// 	HttpHeaders headers = new HttpHeaders();
+	// 	headers.add("Authorization", "Bearer " + accessToken);
+	//
+	// 	HttpEntity<?> entity = new HttpEntity<>(headers);
+	//
+	// 	ResponseEntity<Map> response = restTemplate.exchange(
+	// 		userInfoUri,
+	// 		HttpMethod.GET,
+	// 		entity,
+	// 		Map.class
+	// 	);
+	//
+	// 	return response.getBody();
+	// }
 }
--- src/main/java/org/scoula/backend/domain/kakao/service/KakaoJwtTokenProvider.java
+++ src/main/java/org/scoula/backend/domain/kakao/service/KakaoJwtTokenProvider.java
@@ -9,19 +9,19 @@ import java.util.Date;
 
 @Component
 public class KakaoJwtTokenProvider {
-
-	@Value("${jwt.secret}")
-	private String secretKey;
-
-	// 1시간
-	private final long EXPIRATION = 1000L * 60 * 60;
-
-	public String generateToken(Long userId) {
-		return Jwts.builder()
-			.setSubject(String.valueOf(userId))
-			.setIssuedAt(new Date())
-			.setExpiration(new Date(System.currentTimeMillis() + EXPIRATION))
-			.signWith(SignatureAlgorithm.HS256, secretKey)
-			.compact();
-	}
+	//
+	// @Value("${jwt.secret}")
+	// private String secretKey;
+	//
+	// // 1시간
+	// private final long EXPIRATION = 1000L * 60 * 60;
+	//
+	// public String generateToken(Long userId) {
+	// 	return Jwts.builder()
+	// 		.setSubject(String.valueOf(userId))
+	// 		.setIssuedAt(new Date())
+	// 		.setExpiration(new Date(System.currentTimeMillis() + EXPIRATION))
+	// 		.signWith(SignatureAlgorithm.HS256, secretKey)
+	// 		.compact();
+	// }
 }
